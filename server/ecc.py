f30eaf0793f5cac5f7f42a50db345753d3ae879c
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-11-01 14:49:19 +0700

    Added key generation for ecc

---
+++ server/ecc.py
@@ -0,0 +1,23 @@
+from tinyec import registry
+import secrets
+
+def compress(pubKey):
+    return hex(pubKey.x) + hex(pubKey.y % 2)[2:]
+
+curve = registry.get_curve('brainpoolP256r1')
+
+alicePrivKey = secrets.randbelow(curve.field.n)
+alicePubKey = alicePrivKey * curve.g
+print("Alice public key:", compress(alicePubKey))
+
+bobPrivKey = secrets.randbelow(curve.field.n)
+bobPubKey = bobPrivKey * curve.g
+print("Bob public key:", compress(bobPubKey))
+
+aliceSharedKey = alicePrivKey * bobPubKey
+print("Alice shared key:", compress(aliceSharedKey))
+
+bobSharedKey = bobPrivKey * alicePubKey
+print("Bob shared key:", compress(bobSharedKey))
+
+print("Equal shared keys:", aliceSharedKey == bobSharedKey)
