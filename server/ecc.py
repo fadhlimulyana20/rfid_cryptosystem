bc8598d6fd5d0940322adc3083c993ac479adbf6
Fadhli Mulyana <baba.fadhli@gmail.com>
2022-11-08 14:55:24 +0700

    refactor ecc code to oop

--- server/ecc.py
+++ server/ecc.py
@@ -1,17 +1,31 @@
-from tinyec import registry
+from tinyec import registry, ec
 import secrets
 
 def compress(pubKey):
     return hex(pubKey.x) + hex(pubKey.y % 2)[2:]
 
-curve = registry.get_curve('brainpoolP256r1')
+class Ecc():
+    def __init__(self) -> None:
+        self.curve = registry.get_curve('brainpoolP256r1')
 
-alicePrivKey = secrets.randbelow(curve.field.n)
-alicePubKey = alicePrivKey * curve.g
+    # @staticmethod
+    # def generate_curve():
+    #     return registry.get_curve('brainpoolP256r1')
+
+    def generate_priv_key(self) -> int:
+        return secrets.randbelow(self.curve.field.n)
+
+    def calculate_pub_key(self, priv_key: int):
+        return priv_key * self.curve.g
+
+curve = Ecc()
+
+alicePrivKey = curve.generate_priv_key()
+alicePubKey = curve.calculate_pub_key(alicePrivKey)
 print("Alice public key:", compress(alicePubKey))
 
-bobPrivKey = secrets.randbelow(curve.field.n)
-bobPubKey = bobPrivKey * curve.g
+bobPrivKey = curve.generate_priv_key()
+bobPubKey = curve.calculate_pub_key(bobPrivKey)
 print("Bob public key:", compress(bobPubKey))
 
 aliceSharedKey = alicePrivKey * bobPubKey
